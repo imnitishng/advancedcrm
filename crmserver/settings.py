46bf9766c817db08931c5b2333f34696cf4b0126
Nitish Gupta <imnitish.ng@gmail.com>
2020-10-20 14:46:59 +0530

    Heroku changes

--- crmserver/settings.py
+++ crmserver/settings.py
@@ -128,7 +128,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'yorcrm@gmail.com'
-EMAIL_HOST_PASSWORD = 'yorCRM123@'
+EMAIL_HOST_PASSWORD = 'yorcrm123@'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
